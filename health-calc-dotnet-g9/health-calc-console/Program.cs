bc75d7fbaecbba1e5ac9bb3c8092535b8df025aa
FUNCIONAL\jhonata.santos <jhonata.santos@funcionalcorp.com.br>
2023-05-20 17:01:56 -0300

    Alteração nos testes

--- health-calc-dotnet-g9/health-calc-console/Program.cs
+++ health-calc-dotnet-g9/health-calc-console/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace Health_Calc_Pack.ConsoleApp
 {
     internal class Program
-{
-    static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
     }
-}
 }
